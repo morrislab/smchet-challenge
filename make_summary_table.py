fee76d4bf4cea27e57ef8c07764b188c3287d038
Jeff Wintersinger <jeff.git@wintersinger.com>
2015-06-16 17:38:04 -0400

    Make various small corrections.

--- make_summary_table.py
+++ make_summary_table.py
@@ -2,49 +2,69 @@
 import argparse
 import csv
 import gzip
-import json
 import numpy as np
+import ujson as json
+import sys
+import zipfile
 from collections import defaultdict
 from scipy import stats
 
 def intmode(iter):
   return int(stats.mode(iter)[0][0])
 
-def filter_subclones(subclones):
-  min_ssms = 0
-  return {int(idx): subclone for (idx, subclone) in subclones.items() if int(idx) != 0 and subclone['num_ssms'] >= min_ssms}
+def filter_subclones(subclones, min_ssms):
+  return {
+    int(idx): subclone for (idx, subclone) in subclones.items()
+    if int(idx) != 0 and subclone['num_ssms'] >= min_ssms
+  }
 
-def parse_json(json_fn):
-  with gzip.GzipFile(json_fn) as json_fd:
-    return json.load(json_fd)
+def find_clonal_node(subclones):
+  indices = [k for k in subclones.keys() if k > 0]
+  return min(indices)
 
-def calc_subclone_stats(tree_summary):
-  subclone_counts = []
+def calc_subclone_stats(tree_summary, min_ssms):
+  cancer_pop_counts = []
   cellularities = []
 
-  for tree in tree_summary['trees']:
-    subclones = tree['subclones']
-    subclones = filter_subclones(subclones)
-    subclone_counts.append(len(subclones))
-    cellularities.append(subclones[1]['phi'])
+  for tree_idx, tree in tree_summary.items():
+    try:
+      subclones = tree['subclones']
+      subclones = filter_subclones(subclones, min_ssms)
+      cancer_pop_counts.append(len(subclones))
+      clonal_idx = find_clonal_node(subclones)
+      cellularities.append(subclones[clonal_idx]['phi'])
+    except:
+      from IPython import embed
+      embed()
+  return (intmode(cancer_pop_counts) - 1, np.mean(cellularities))
 
-  return (intmode(subclone_counts), np.mean(cellularities))
-
-def calc_frac_clonal(mut_assignments):
+def calc_frac_clonal(mut_assignments, tree_summary, min_ssms):
   clonal_counts = defaultdict(int)
   subclonal_counts = defaultdict(int)
   ssm_ids = []
 
-  for tree in mut_assignments['trees']:
-    for subclone_idx, features in tree.items():
-      is_clonal = (subclone_idx == '1')
-      for ssm in features['ssms']:
-        ssm_id = ssm['id']
-        ssm_ids.append(ssm_id)
-        if is_clonal:
-          clonal_counts[ssm_id] += 1
-        else:
-          subclonal_counts[ssm_id] += 1
+  with zipfile.ZipFile(mut_assignments) as mutf:
+    for zinfo in mutf.infolist():
+      tree_idx = zinfo.filename.split('.')[0]
+      subclones = filter_subclones(tree_summary['trees'][tree_idx]['subclones'], min_ssms)
+      clonal_idx = find_clonal_node(subclones)
+
+      tree_assignments_json = mutf.read(zinfo)
+      tree_assignments = json.loads(tree_assignments_json)
+
+      for subclone_idx, muts in tree_assignments.items():
+        if subclone_idx == 'dataset_name':
+          continue
+        subclone_idx = int(subclone_idx)
+        is_clonal = (subclone_idx == 1)
+
+        for ssm in muts['ssms']:
+          ssm_id = ssm['id']
+          ssm_ids.append(ssm_id)
+          if is_clonal:
+            clonal_counts[ssm_id] += 1
+          else:
+            subclonal_counts[ssm_id] += 1
 
   num_clonal_ssms = 0
   num_subclonal_ssms = 0
@@ -66,8 +86,14 @@ def get_cancer_type(dataset_name, pancancer_tsv_details):
         return cancer_type
   raise Exception('Could not find cancer type for dataset %s' % dataset_name)
 
+def count_ssms(tree_summary):
+  total_ssms = sum([s['num_ssms'] for s in tree_summary['trees']['0']['subclones'].values()])
+  return total_ssms
+
 def main():
   parser = argparse.ArgumentParser(description='Hooray')
+  parser.add_argument('--min-ssms', dest='min_ssms', type=int, default=3,
+    help='Minimum number of SSMs to retain a subclone')
   parser.add_argument('tree_summary',
     help='JSON-formatted tree summaries')
   parser.add_argument('mutation_assignment',
@@ -79,14 +105,19 @@ def main():
     help='TSV file listing sample details')
   args = parser.parse_args()
 
-  tree_summary = parse_json(args.tree_summary)
-  mut_assignments = parse_json(args.mutation_assignment)
+  with gzip.GzipFile(args.tree_summary) as json_fd:
+    tree_summary = json.load(json_fd)
+
+  total_ssms = count_ssms(tree_summary)
+  if total_ssms < 10:
+    print >> sys.stderr, 'Only %s SSMs. Exiting.' % total_ssms
+    return
 
   dataset_name = tree_summary['dataset_name']
   cancer_type = get_cancer_type(dataset_name, args.pancancer_tsv_details)
-  num_subclones, cellularity = calc_subclone_stats(tree_summary)
+  num_subclones, cellularity = calc_subclone_stats(tree_summary['trees'], args.min_ssms)
   ploidy = 'NA'
-  frac_clonal = calc_frac_clonal(mut_assignments)
+  frac_clonal = calc_frac_clonal(args.mutation_assignment, tree_summary, args.min_ssms)
   branching = 'NA'
 
   print('\t'.join([str(v) for v in (cancer_type, dataset_name, num_subclones, cellularity, ploidy, frac_clonal, branching)]))
